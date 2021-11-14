1a76452690e8d3023328cba3c055586f0b65fa8b
nagye99 <nagye99@gmail.com>
2021-11-14 19:15:16 +0100

    User sign up and sign in methods

---
+++ src/main/java/database/HandleData.java
@@ -0,0 +1,63 @@
+package database;
+
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class HandleData {
+    private static String databaseDirectory = System.getProperty("user.home") + File.separator+ ".users_data";
+
+    private static void MakeUsersDir() {
+        try{
+            Path databaseDirectoryPath = Path.of(databaseDirectory);
+            if (Files.notExists(databaseDirectoryPath)){
+                Files.createDirectory(databaseDirectoryPath);}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Jdbi CreateConnection(){
+        MakeUsersDir();
+        String databaseURL = databaseDirectory + File.separator + "data";
+        Jdbi jdbi = Jdbi.create("jdbc:h2:file:"+ databaseURL);
+        jdbi.installPlugin(new SqlObjectPlugin());
+        return jdbi;
+    }
+
+    public static boolean saveUser(User user){
+        Jdbi jdbi = CreateConnection();
+        try(Handle handle = jdbi.open()) {
+            UsersDao dao = handle.attach(UsersDao.class);
+            dao.createTable();
+            List<String> usernames= dao.getUsernames();
+            if(usernames.contains(user.username)){
+                System.out.println("Már létezik a felhasználónév");
+                return false;
+            }else{
+                dao.insertUser(user);
+                return true;
+            }
+        }
+    }
+
+    public static boolean checkUser(String username, String password){
+        Jdbi jdbi = CreateConnection();
+        try(Handle handle = jdbi.open()){
+            UsersDao dao = handle.attach(UsersDao.class);
+            List<String> usernames= dao.getUsernames();
+            if(usernames.contains(username)){
+                return dao.getPassword(username).equals(password);
+            }else{
+                System.out.println("Nem létező felhasználó");
+                return false;
+            }
+        }
+    }
+}
---
+++ src/main/java/database/UsersDao.java
@@ -0,0 +1,32 @@
+package database;
+
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+import java.util.List;
+
+@RegisterBeanMapper(User.class)
+public interface UsersDao {
+    @SqlUpdate("""
+        CREATE TABLE IF NOT EXISTS USERS (
+            id IDENTITY PRIMARY KEY,
+            username VARCHAR NOT NULL UNIQUE,
+            password VARCHAR NOT NULL
+        )
+        """
+    )
+    void createTable();
+    @SqlUpdate("INSERT INTO USERS (username, password) VALUES (:username, :password)")
+    @GetGeneratedKeys
+    long insertUser(@BindBean User user);
+
+    @SqlQuery("SELECT username FROM users")
+    List<String> getUsernames();
+
+    @SqlQuery("SELECT password FROM users WHERE username = :username")
+    String getPassword(@Bind("username") String username);
+}

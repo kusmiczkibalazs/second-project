608ceb90f970b48439fdf254ad470874588a6c8a
Admadma <admadmaa77@gmail.com>
2021-11-17 19:11:49 +0100

    temp changes

--- src/main/java/manager/model/ManagerModel.java
+++ src/main/java/manager/model/ManagerModel.java
@@ -2,6 +2,9 @@ package manager.model;
 
 import manager.database.HandleData;
 import manager.database.User;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.util.Random;
 
 public class ManagerModel {
 
@@ -35,5 +38,38 @@ public class ManagerModel {
         String decoded = original;
 
         return decoded;
+    }
+
+    public static void givenUsingApache_whenGeneratingRandomStringBounded_thenCorrect() {
+
+        int length = 10;
+        boolean useLetters = true;
+        boolean useNumbers = false;
+        char[] chars = {'$', '#', 'Ł'};
+        //String generatedString = RandomStringUtils.random(length, useLetters, useNumbers);
+
+        String test = RandomStringUtils.random(10, 0, chars.length-1, true, true, chars);
+        System.out.println(test);
+    }
+
+    public void generatePassword(int length, boolean capital, boolean numbers, boolean special){
+
+
+    }
+
+    public static String generateRandomPassword(int len) {
+        String chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghi"
+                + "jklmnopqrstuvwxyz!@#$%&";
+        Random rnd = new Random();
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++)
+            sb.append(chars.charAt(rnd.nextInt(chars.length())));
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        //givenUsingApache_whenGeneratingRandomStringBounded_thenCorrect();
+
+        //System.out.println(generateRandomPassword(4));
     }
 }
